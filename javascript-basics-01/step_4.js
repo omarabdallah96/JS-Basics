78ed59d8a7204258371bba771e845a84bc8f8b29
omar abdallah <omarlb512@gmail.com>
2021-03-13 15:07:10 +0200

    Step_4 complete

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,5 @@
+var name=prompt("Enter your name: ");
+var surname=prompt("Enter your surname: ");
+var city=prompt("Enter your city: ");
+
+window.alert( "hello"+"\n"+"name:"+name+"\nsurname:"+surname+"\ncity:"+city);
