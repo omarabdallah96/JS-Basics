b7c1def37c27e4678386ab73a8d6f8b104bfcfaf
omar abdallah <omarlb512@gmail.com>
2021-03-14 12:08:58 +0200

    basic 2 step 4 complete

---
+++ javascript-basics-02/step 4/step_4.js
@@ -0,0 +1,11 @@
+var butt=document.querySelector("button");
+butt.addEventListener("click",reset);
+
+function reset(){
+    if (confirm("Are you sure?")){
+        var n=document.querySelector("#name").value="";
+        var s=document.querySelector("#surname").value="";
+        var c=document.querySelector("#city").value="";
+ }
+
+}
